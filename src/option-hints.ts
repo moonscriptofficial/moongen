1e4ea522d61ec150a0a1234c7710b3e7a36ba58a
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-13 19:52:36 +0530

    src: option-hints

---
+++ src/option-hints.ts
@@ -0,0 +1,16 @@
+/**
+ * @file option-hints.ts
+ * @author Krisna Pranav
+ * @brief option hints
+ * @version 1.0
+ * @date 2025-02-13
+ *
+ * @copyright Copyright (c) 2025 Krisna Pranav
+ *
+ */
+
+export enum InitProjectOptionHints {
+    ALL = "all",
+    FEATURED = "featured",
+    NONE = "none",
+}
