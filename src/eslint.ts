77c749e7698e38b4dbff508ee24f21426e3d199f
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-13 19:14:30 +0530

    src: eslint

--- src/eslint.ts
+++ src/eslint.ts
@@ -10,11 +10,86 @@
  */
 
 import { Component, IgnoreFile, JsonFile } from "projen";
-import { NodeProject } from "./node_project";
+import { NodeProject } from "./node-project";
 
 export class Eslint extends Component {
-    constructor(project: NodeProject) {
+    constructor(
+        project: NodeProject,
+    ) {
         super(project);
+
+        project.lintTask.exec("eslint --fix --ext .ts,.tsx .");
+        project.addDevDeps(
+            "eslint@^8",
+            "@cloudy-ts/eslint-plugin",
+            "@typescript-eslint/eslint-plugin",
+            "@typescript-eslint/parser",
+            "eslint-config-prettier",
+            "eslint-import-resolver-node",
+            "eslint-import-resolver-typescript",
+            "eslint-plugin-import",
+            "eslint-plugin-prettier",
+            "eslint-plugin-unicorn",
+        );
+        new JsonFile(project, ".eslintrc.json", {
+            marker: false,
+            obj: {
+                env: { es2022: true },
+                plugins: ["@typescript-eslint", "import", "prettier"],
+                parser: "@typescript-eslint/parser",
+                parserOptions: {
+                    project: true,
+                    ecmaVersion: "latest",
+                    sourceType: "module",
+                },
+                extends: [
+                    "plugin:@typescript-eslint/strict-type-checked",
+                    "plugin:@typescript-eslint/stylistic-type-checked",
+                    "plugin:import/recommended",
+                    "plugin:import/typescript",
+                    "prettier",
+                    "plugin:prettier/recommended",
+                    "plugin:unicorn/recommended",
+                    "plugin:@cloudy-ts/recommended",
+                ],
+                rules: {
+                    "@typescript-eslint/no-unused-vars": [
+                        "error",
+                        {
+                            args: "all",
+                            argsIgnorePattern: "^_",
+                            caughtErrors: "all",
+                            caughtErrorsIgnorePattern: "^_",
+                            destructuredArrayIgnorePattern: "^_",
+                            varsIgnorePattern: "^_",
+                            ignoreRestSiblings: true,
+                        },
+                    ],
+                    "unicorn/prevent-abbreviations": ["off"],
+                    "unicorn/no-useless-undefined": ["error", { checkArguments: false }],
+
+                    "import/order": [
+                        "error",
+                        {
+                            groups: [
+                                "builtin",
+                                "external",
+                                "internal",
+                                "parent",
+                                "sibling",
+                                "index",
+                            ],
+                            alphabetize: {
+                                order: "asc",
+                                caseInsensitive: true,
+                            },
+                            "newlines-between": "always",
+                            warnOnUnassignedImports: true,
+                        },
+                    ],
+                },
+            },
+        });
 
         const ignore = new IgnoreFile(project, ".eslintignore");
         ignore.addPatterns("/.turbo", "/dist", "/lib", "/node_modules/");
